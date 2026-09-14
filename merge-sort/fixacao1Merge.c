98a7639a5d83bfbb63610a70a9c879ebf82a0b30
Graziela Ralph <ralphgraziela25@gmail.com>
2026-09-13 22:39:58 -0400

    update
    ajeitei umas coisas e descobri que a gambiarra que eu tinha feito tava errada rs

--- merge-sort/fixacao1Merge.c
+++ merge-sort/fixacao1Merge.c
@@ -1,27 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+
+int* criarVetor (int n){
+    int *vetor = (int *) malloc(n * sizeof(int)); //alocacao dinamica
+
+    for(int i = 0; i < n; i++){
+        vetor[i] = rand() % 1000; //atribui valores aleatorios pro vetor de 0 a 999
+    }
+
+    return vetor;
+}
+
+void printVetor (int *num, int n){
+     for(int i = 0; i < n; i++){
+        printf(" %d ", num[i]);
+    } 
+    printf("\n");
+}
+
+//--------------------------------------------------------------------------------------------
 
 /*p -> posicao inicial do subvetor
 q -> mediana do vetor
 r -> posicao final do subvetor*/
 
+int* left(int *num, int p, int q){
+	int fim = q-p+1;
 
-void sortByMerge(int *num, int n, int p, int q, int r){
-	
+	int *L = (int *) malloc((fim + 1) * sizeof(int)); //alocacao dinamica do vetor
+
+	for(int i = 0; i < fim; i++){
+		L[i] = num[p+i];
+	}
+	L[fim] = INT_MAX;
+	return L;
 }
 
-void merge(int *num, int n, int p, int q, int r){
-	int qtdL = (n/2)+1;
-	int qtdR = n/2;
-	int *L = (int *) malloc(qtdL * sizeof(int));
-	int *R = (int *) malloc(qtdR * sizeof(int));
+int* right(int *num, int q, int r){
+	int fim = r-q;
 
-	L = left(num, n, p, q, r);
-	R = right(num, n, p, q, r);
+	int *R = (int *) malloc ((fim + 1) * sizeof(int));
+
+	for(int i = 0; i < fim; i++){
+		R[i] = num[q+i+1];
+	}
+
+	R[fim] = INT_MAX;
+	return R;
+}
+
+void merge(int *num, int p, int q, int r){
+	int *L = left(num, p, q); 
+	int *R = right(num, q, r);//alocacao dinamica e feita dentro das funcoes left e right
 	int i = 0;
 	int j = 0;
-
 
 	for(int k = p; k <= r; k++){
 		if (L[i] <= R[j]){
@@ -32,21 +66,42 @@ void merge(int *num, int n, int p, int q, int r){
 			j++;
 		}
 	}
+
+	free(L);
+	free(R);
 	
 }
 
-int* left(int *num, int n, int p, int q, int r){
-	fim = q-p+1;
+void sortByMerge(int *num, int p, int r){
+	if (p < r){
+		int q = (p+r)/2;
+		sortByMerge(num, p, q);
+		sortByMerge(num, q+1, r);
+		merge(num, p, q, r);
+	}
 
-	//for(int i = 1; )
-	return *L;
 }
 
-int* right(int *num, int n, int p, int q, int r){
-	return *R;
-}
+
+//------------------------------------------------------------------------------------------------------------
 
 int main(){
+	srand(time(NULL));
+
+    int n = 0;
+    printf("Digite tamanho do vetor: ");
+    scanf("%d", &n);
+    int *vetor = criarVetor(n);
+
+    printf("\n");
+    printVetor(vetor, n);
+    printf("\n");
+	
+	sortByMerge(vetor, 0, n-1);
+    printVetor(vetor, n);
+
+    free(vetor);
+
 
 	return 0;
 }
